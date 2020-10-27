d05deff1fd6848d96ba775cf3b29ecab5246a789
sungjun030303 <sjkim030303@yahoo.co.jp>
2020-10-27 16:02:23 +0900

    돔 선언후 이거저거 됨

--- src/App.js
+++ src/App.js
@@ -4,28 +4,26 @@
 import { render } from "@testing-library/react";
 
 function App() {
-  return (
-    <h1>
-      <header >
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload. reload good
-        </p>
-      </header>
-      helloworld
-{/* div tagㄱㅏ jsx html같은식으로 되어있다.
-        왜 이걸 사용하는가.. 토란스파일 -> 자바스크립트 코드로 변경되어진다.
-         대충 밑의 느낌으로 코딩인듯 현 버전은 안돔 jsx가 직관적 코딩이 가능하다.*/}
-    </h1>
-    
-  );
+  // tag　를 존재 하지 않게 하기위한 React.Fragment도 있다.
+const inputtext =
+<React.Fragment> <label> bar </label>
+<input type="text" onClick= { ()=> {console.log("I am clicked.")}   }/>
+</React.Fragment>;
 
-  // render() {
-  // React.createElment (        
-  //   "div",
-  //   null,
-  //   "Hello World") 
-  // }
+const greeting = "hello";
+const dom = <h1>
+<header >
+  {/* <img src={logo} className="App-logo" alt="logo" /> */}
+  <p>
+    Edit <code>src/App.js</code> and save to reload. reload good
+  </p>
+</header>
+{greeting}world 
+{/* div tagㄱㅏ jsx html같은식으로 되어있다.
+  왜 이걸 사용하는가.. 토란스파일 -> 자바스크립트 코드로 변경되어진다.
+   대충 밑의 느낌으로 코딩인듯 현 버전은 안돔 jsx가 직관적 코딩이 가능하다.*/}
+</h1>;
+  return inputtext;
 
 }
 
