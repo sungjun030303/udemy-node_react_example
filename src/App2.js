1579691f79ca942585d5f5a45858c1a2e3910a86
sungjun030303 <sjkim030303@yahoo.co.jp>
2020-10-27 17:17:48 +0900

    components & props

---
+++ src/App2.js
@@ -0,0 +1,36 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import { render } from "@testing-library/react";
+
+const App = () => {
+const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName"  }
+  ];
+
+  return (
+    <div>
+      {
+        profiles.map ((profile, index) => {
+            return <User name={profile.name} age={profile.age}  key={index}/>
+        })
+      }
+    </div>
+  )
+}
+
+// const Cat =() => {
+//     return <div>Meow!</div>    
+// }
+
+const User =( props ) => {
+    return <div>Hi , I am {props.name} , and I am {props.age} years old</div>    
+}
+
+User.defaultProps = {
+    age: 1
+}
+
+export default App;
