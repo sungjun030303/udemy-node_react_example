6d0ea44771edb97cccdb2211fd941a65eca7b3b0
sungjun030303 <sjkim030303@yahoo.co.jp>
2020-10-27 17:58:52 +0900

    class & construct & state 사용법을 배움

---
+++ src/App4.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react';
+
+const App =() => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor (props) {
+    super(props)
+    console.log(this.state)
+    this.state = { count : 0}
+  }
+  handlePushButton =()=> {
+    console.log(this.state.count)
+    this.setState( { count : this.state.count+1 } );
+  }
+
+  handleMinusButton =()=> {
+    console.log(this.state.count)
+    this.setState( {count : this.state.count-1 });
+  }
+
+  render() {
+    console.log(this.state);
+
+    return ( 
+    <React.Fragment>
+    <div> count: {this.state.count} </div> 
+    <button onClick={this.handlePushButton}>+1</button>
+    <button onClick={this.handleMinusButton}>-1</button>
+    </React.Fragment> 
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App3';
+import App4 from './App4';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App4 />
   </React.StrictMode>,
   document.getElementById('root')
 );
