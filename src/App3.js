6b5e265f147d6dc6d21f72d851d7d762f7bbf37f
sungjun030303 <sjkim030303@yahoo.co.jp>
2020-10-27 17:31:41 +0900

    prop-type.js

---
+++ src/App3.js
@@ -0,0 +1,42 @@
+//import logo from './logo.svg';
+//import './App.css';
+import PropTypes from 'prop-types';
+
+
+const App = () => {
+const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "PropTyeps check"  }
+  ];
+
+  return (
+    <div>
+      {
+        profiles.map ((profile, index) => {
+            return <User name={profile.name} age={profile.age}  key={index}/>
+        })
+      }
+    </div>
+  )
+}
+
+// const Cat =() => {
+//     return <div>Meow!</div>    
+// }
+
+const User =( props ) => {
+    return <div>Hi , I am {props.name} , and I am {props.age} years old</div>    
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
+}
+
+User.defaultProps = {
+    age: 1
+}
+
+
+export default App;
