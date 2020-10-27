77bb8371bc97b92f3261bea3e75493df3953c0ca
sungjun030303 <sjkim030303@yahoo.co.jp>
2020-10-27 18:20:47 +0900

    action의 작성......  type을 정의해놓는것.

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const decrement = () => ({
+    type: DECREMENT
+})
+
+// export const decrement = () => {
+//     return 
+//     {
+//         type: 'DECREMENT'
+//     }
+// }
+
